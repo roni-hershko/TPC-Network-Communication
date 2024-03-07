0ae403f55a0c5d6af6f840a3f75224065bf2f3b2
Gal Patel <galpat@post.bgu.ac.il>
2024-03-07 15:41:51 +0200

    gp in client

--- client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
+import bgu.spl.net.impl.tftp.TftpProtocol;
+import bgu.spl.net.impl.tftp.TftpEncoderDecoder;
+
 	
 	public class TftpClient {
 	
 		public static void main(String[] args) throws IOException {
+
 	
 			if (args.length == 0) {
 				args = new String[]{"localhost", "hello"};
@@ -18,20 +23,58 @@ import java.net.Socket;
 				System.out.println("you must supply two arguments: host, message");
 				System.exit(1);
 			}
-	
+
+			TftpProtocol protocol = new TftpProtocol();
+			TftpEncoderDecoder encdec = new TftpEncoderDecoder();
+
 			//BufferedReader and BufferedWriter automatically using UTF-8 encoding
 			try (Socket sock = new Socket(args[0], 7777);
 					BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 					BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()))) {
-	
+				
 				System.out.println("sending message to server");
-				out.write(args[1]);
-				out.newLine();
-				out.flush();
+				Object lock = new Object();
+
+				Thread KeyboardThread = new Thread(() -> {
+					BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+					while (true) {
+						try {
+							String line = keyboard.readLine();	
+							synchronized (lock) {
+								out.write(new String(encdec.encode(protocol.creatRequest(line))));
+								out.newLine();
+								out.flush();
+							}
+							}
+						catch (IOException e) {
+							e.printStackTrace();
+						}
+						lock.notify();
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+				KeyboardThread.start();
+
+				Thread ListenThread = new Thread(() -> {
+					while (true) {
+						try {
+							String line = in.readLine();
+							byte[] lineToByte = line.getBytes();
+							for (int i = 0; i < lineToByte.length; i++) {
+								protocol.process(encdec.decodeNextByte(lineToByte[i]));
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						lock.notify();
+					}
+				});
 	
-				System.out.println("awaiting response");
-				String line = in.readLine();
-				System.out.println("message from server: " + line);
+				ListenThread.start();
 			}
 		}
 	}
